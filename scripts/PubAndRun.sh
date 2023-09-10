d7c522c0fc93dd505a6276a1a52fb9cde83643ab
walt <2963975+waltiam@users.noreply.github.com>
2023-09-10 02:34:18 +0000

    feat(dotnet7): still not a good script, it works

--- scripts/PubAndRun.sh
+++ scripts/PubAndRun.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 echo "Publishing:"
-dotnet publish # > dev/null
+dotnet publish 
 echo "Deploying:"
-set cwd = pwd
+set cwd=$(pwd)
 #
-mv --force  ./usage/Floatingman.CommandLine.Application/bin/Debug/net7.0/publish/*.* ~/tmp/cmd # > dev/null
-mv --force  ./usage/Floatingman.CommandLine.Command/bin/Debug/net7.0/publish/*.* ~/tmp/cmd/plugins/GenerateHexArray # > dev/null
+mv --force  ./usage/Floatingman.CommandLine.Application/bin/Debug/net7.0/publish/*.* ~/tmp/cmd
+mv --force  ./usage/Floatingman.CommandLine.Command/bin/Debug/net7.0/publish/*.* ~/tmp/cmd/plugins/GenerateHexArray
 #
 cd ~/tmp/cmd
 echo "Running: [Floatingman.CommandLine.Application GenerateHexArray -U 2 -V 2 --radius 10]"
 echo "~"
-./Floatingman.CommandLine.Application.exe GenerateHexArray -U 2 -V 2 --radius 10.0
+./Floatingman.CommandLine.Application GenerateHexArray -U 2 -V 2 --radius 10.0
 echo "~"
 cd $cwd
